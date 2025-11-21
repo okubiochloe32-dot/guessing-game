115cc4dba26acd5794777684a83d5243cd0a3d16
Chloe <okubiochloe32@gmail.com>
2025-11-20 23:45:01 -0800

    added a README file

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a guessing game app made to be really tricky but fun.
---
+++ index.js
@@ -0,0 +1,30 @@
+const playerInput = document.querySelector("#playerInput");
+const submitBtn = document.querySelector("#submitBtn");
+const result = document.querySelector("#result");
+
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(`Generated Random Number: ${randomNumber}`);
+
+submitBtn.addEventListener("click", () => 
+{
+    const userGuess = Number(playerInput.value);
+
+    console.log(`Player Entered: ${userGuess}`)
+
+    if (userGuess < 1 || userGuess > 100 || isNaN(userGuess)) {
+        result.textContent = "Play this game and enter a valid number between 1 and 100.";
+        console.log( `Invalid input: ${userGuess}`)
+        return;
+    } 
+    
+    if (userGuess === randomNumber) {
+        result.textContent = "Congratulations!! You WON ₦2,000,000!!";
+        result.style.color = "white";
+        console.log( `Correct! Player won ₦2,000,000 with guess ${userGuess}`)
+    } else {
+        result.textContent = `You lost this round. Try again to have a chance to win!`;
+        console.log( `Wrong guess ${userGuess}. Try again.`)
+    }
+});
+
